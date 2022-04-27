96e362f2936e20a2f57afb83748fac3a96dfe34a
brian corker <bcorker123@gmail.com>
2022-04-27 17:44:34 -0400

    commit tree 3bd72ff72fb718f8dd292370657b36023e6e1b26

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 // code your solution here
+// const record = [
+//     { year: "2018", result: "N/A" },
+//     { year: "2017", result: "N/A" },
+//     { year: "2016", result: "N/A" },
+//     { year: "2015", result: "W" }
+// ]
+// const finder = () => record.find( ({result}) => result === 'W')
+
+// const superbowlWin = (record) => {
+//     if (finder()) {
+//         let winningYear = finder();
+//         return winningYear['year'];
+//     } else {
+//         return undefined;
+//     }
+// }
+
+const superbowlWin = (record) => {
+    if (record.find( ({result}) => result === 'W')) {
+        let winningYear = record.find( ({result}) => result === 'W');
+        return winningYear['year'];
+    } else {
+        return undefined;
+    }
+}
